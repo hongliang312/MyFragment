e08b994d6eddd295fcddb016e2208f586713a08f
zhangsan <zhangsan@bawei>
2018-04-27 17:53:49 +0800

    Initial commit

---
+++ app/src/main/java/com/lightheart/sphr/myfragment/MainActivity.java
@@ -0,0 +1,65 @@
+package com.lightheart.sphr.myfragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.lightheart.sphr.myfragment.fragment.HomeFragment;
+import com.lightheart.sphr.myfragment.fragment.ThreeFragment;
+import com.lightheart.sphr.myfragment.fragment.TwoFragment;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    private TextView tv_nearby;
+    private TextView tv_recomment;
+    private TextView tv_tochen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       setFragment(new HomeFragment());
+
+       intiData();
+       oclick();
+    }
+
+    private void oclick() {
+        tv_nearby.setOnClickListener(this);
+        tv_recomment.setOnClickListener(this);
+        tv_tochen.setOnClickListener(this);
+    }
+
+    private void intiData() {
+        tv_nearby = findViewById(R.id.tv_nearby);
+        tv_recomment = findViewById(R.id.tv_recomment);
+        tv_tochen = findViewById(R.id.tv_aa);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_recomment:
+                setFragment(new HomeFragment());
+                break;
+            case R.id.tv_nearby:
+                setFragment(new TwoFragment());
+                break;
+            case R.id.tv_aa:
+                setFragment(new ThreeFragment());
+
+        }
+    }
+
+
+    /**
+     * 切换Fragment
+     */
+    private void setFragment(Fragment f) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.home_page_fragment,f).commit();
+    }
+
+
+}
---
+++ app/src/main/java/com/lightheart/sphr/myfragment/fragment/TwoFragment.java
@@ -0,0 +1,26 @@
+package com.lightheart.sphr.myfragment.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lightheart.sphr.myfragment.R;
+
+/**
+ * Created by 知足 on 2018/4/27.
+ */
+
+public class TwoFragment extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.twofragment, null, false);
+        return view;
+    }
+}
